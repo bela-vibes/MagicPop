3f338ac0c08824ed7c469340dc8d230be8b81bae
bela-vibes <bela@magicpop.berlin>
2026-04-07 07:36:49 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+export default {
+  content: [
+    "./index.html",
+    "./**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        'magic-black': '#000000',
+        'magic-dark': '#000000',
+        'off-white': '#F9F7F2',
+        'magic-orange': '#FF4D00',
+        'magic-blue': '#0038FF',
+        'magic-pink': '#FFB7D5',
+      },
+      fontFamily: {
+        archivo: ['Archivo Black', 'sans-serif'],
+        editorial: ['Instrument Serif', 'serif'],
+        inter: ['Inter', 'sans-serif'],
+      },
+      animation: {
+        'fade-in': 'fadeIn 0.5s ease-out both',
+      },
+      aspectRatio: {
+        '3/2': '3 / 2',
+      }
+    }
+  },
+  plugins: [],
+}
