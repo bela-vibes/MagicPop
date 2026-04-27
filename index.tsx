bd5da81dc8901256ab4b514d43a6637636529dde
bela-vibes <bela@magicpop.berlin>
2026-04-27 10:21:26 +0200

    Remove BrowserRouter from App rendering
    Removed BrowserRouter wrapper from App component.

--- index.tsx
+++ index.tsx
@@ -1,7 +1,6 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
@@ -13,8 +12,6 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
