d823b255026d315da2f3402df47383bbcc10eba8
bela-vibes <bela@magicpop.berlin>
2026-05-11 09:20:02 +0200

    Add build target and cssTarget for Safari 14

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,10 @@ export default defineConfig(({ mode }) => {
         port: 3000,
         host: '0.0.0.0',
       },
+      build: {
+        target: ['es2015', 'safari14'],
+        cssTarget: 'safari14',
+      },
       plugins: [react()],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
